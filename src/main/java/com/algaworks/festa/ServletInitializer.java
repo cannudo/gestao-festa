7ee08c4d638105608ec9fd9adce4c9206a5619b8
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-10-04 11:47:21 -0300

    :memo: comenta o código

--- src/main/java/com/algaworks/festa/ServletInitializer.java
+++ src/main/java/com/algaworks/festa/ServletInitializer.java
@@ -7,7 +7,15 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GestaoFestaApplication.class);
+		Class<?> classeDaAplicacao = GestaoFestaApplication.class;
+		
+		/* :raised_hand: Pra quê essa perfumaria toda, Cannudo?
+		 *
+		 * :nerd_face: Para que a aplicação rode independente do contexto e do ambiente.
+		 * :light_bulb: Repare no retorno da função.
+		 */
+		
+		return application.sources(classeDaAplicacao);
 	}
 
 }
