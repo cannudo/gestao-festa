c36a925a49a10b7fff1218baba2bba8449fe40ae
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-10-07 17:25:30 -0300

    :white_check_mark: request com payload carregado

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package com.algaworks.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.festa.model.Convidado;
 import com.algaworks.festa.repository.Convidados;
 
 @Controller
@@ -15,10 +17,17 @@ public class ConvidadosController {
 	@RequestMapping("/convidados")
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("ListaConvidados");
+		mv.addObject(new Convidado());
 		mv.addObject("convidados", convidados.todos());
 		return mv;
 	}
 
+	@RequestMapping(value = "/convidados", method = RequestMethod.POST)
+	public String salvar(Convidado novoConvidado) {
+		this.convidados.adicionar(novoConvidado);
+		return "redirect:/convidados";
+	}
+	
 	public String listarLegado() {
 		/* :thinking_face: Por que a string deve ter exatamente o mesmo nome do arquivo HTML?
 		 * Google pesquisar.
